a9c32a7f0e1c09d5c7ba60abb951981c547b179e
RedSparr0w <admin@redsparr0w.com>
2021-04-04 15:35:27 +1200

    Update Attacks.ts

--- src/js/Attacks.ts
+++ src/js/Attacks.ts
@@ -1,17 +1,45 @@
 import { PokemonType } from './PokemonList';
 
-type Attack = {
+export enum AttackType {
+  physical,
+  special,
+  ability,
+}
+
+export type Attack = {
   name: string,
+  type: AttackType,
   duration: number,
   cooldown: number,
 };
 
-const Attacks: Record<PokemonType, Attack> = {
-  [PokemonType.Normal]: {
-    name: 'cut',
-    duration: 750,
-    cooldown: 1000,
-  },
+const Attacks: Record<PokemonType, Attack[]> = {
+  [PokemonType.None]: [],
+  [PokemonType.Normal]: [
+    {
+      name: 'cut',
+      type: AttackType.physical,
+      duration: 750,
+      cooldown: 1000,
+    },
+  ],
+  [PokemonType.Fire]: [],
+  [PokemonType.Water]: [],
+  [PokemonType.Electric]: [],
+  [PokemonType.Grass]: [],
+  [PokemonType.Ice]: [],
+  [PokemonType.Fighting]: [],
+  [PokemonType.Poison]: [],
+  [PokemonType.Ground]: [],
+  [PokemonType.Flying]: [],
+  [PokemonType.Psychic]: [],
+  [PokemonType.Bug]: [],
+  [PokemonType.Rock]: [],
+  [PokemonType.Ghost]: [],
+  [PokemonType.Dragon]: [],
+  [PokemonType.Dark]: [],
+  [PokemonType.Steel]: [],
+  [PokemonType.Fairy]: [],
 };
 
 export default Attacks;
