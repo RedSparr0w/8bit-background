492f81bd6249284315c3f325229c5897402df269
Danial <admin@redsparr0w.com>
2021-04-08 19:10:35 +1200

    Update menu

--- src/js/Menu.ts
+++ src/js/Menu.ts
@@ -1,10 +1,10 @@
 const menu = document.getElementById('menu');
 
 document.getElementById('openMenu').onclick = (): void => {
-  menu.style.width = '250px';
+  menu.style.left = '0px';
 };
 
 /* Set the width of the side navigation to 0 */
 document.getElementById('closeMenu').onclick = (): void => {
-  menu.style.width = '0px';
+  menu.style.left = '-250px';
 };
