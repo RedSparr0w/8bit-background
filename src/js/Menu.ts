ed9346e7bd3f6989f4761dcd5c11700a84b91e8f
Danial <admin@redsparr0w.com>
2021-04-11 16:35:27 +1200

    Allow changing between fighting and passive pokemon

--- src/js/Menu.ts
+++ src/js/Menu.ts
@@ -1,10 +1,34 @@
+import BackgroundPokemon from './BackgroundPokemon/BackgroundPokemon';
+import ActivePokemon from './BattlePokemon/ActivePokemon';
+
 const menu = document.getElementById('menu');
 
 document.getElementById('openMenu').onclick = (): void => {
   menu.style.left = '0px';
 };
 
-/* Set the width of the side navigation to 0 */
 document.getElementById('closeMenu').onclick = (): void => {
   menu.style.left = '-250px';
 };
+
+document.getElementById('pokemonDisplay').onchange = (e): void => {
+  const val = (e.target as HTMLSelectElement).value;
+  BackgroundPokemon.stop();
+  ActivePokemon.clearAll();
+  switch (val) {
+    case 'passive':
+      BackgroundPokemon.start();
+      break;
+    case 'active':
+      for (let i = 4; i >  0; i--) {
+        ActivePokemon.add();
+      }
+      break;
+  }
+
+  // Save the last selected option in settings/local storage
+  localStorage.pokemonDisplay = val;
+};
+
+(document.getElementById('pokemonDisplay') as HTMLSelectElement).value = localStorage.pokemonDisplay || 'none';
+(document.getElementById('pokemonDisplay') as HTMLSelectElement).dispatchEvent(new Event('change'));
